6c60b360afec2986c4a2ec3b3895db0f5c97b143
anddev404 <marcinek07@gmail.com>
2023-04-16 11:27:12 +0200

    add Login Activity

--- app/src/main/java/com/example/myemptyviews/MainActivity.kt
+++ app/src/main/java/com/example/myemptyviews/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.myemptyviews
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var loginImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         supportActionBar?.title = "Wybierz aktywność"
+
+        initializeViews()
+        setOnClickListeners()
+
+    }
+
+    fun initializeViews() {
+        loginImageView = findViewById(R.id.imageView1)
+    }
+
+    fun setOnClickListeners() {
+        loginImageView.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
