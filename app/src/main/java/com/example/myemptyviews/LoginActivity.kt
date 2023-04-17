ee0a950048c1f2ae2cba8184c6f49331c1cfb6c9
anddev404 <marcinek07@gmail.com>
2023-04-17 21:04:20 +0200

    update login activity

--- app/src/main/java/com/example/myemptyviews/LoginActivity.kt
+++ app/src/main/java/com/example/myemptyviews/LoginActivity.kt
@@ -2,11 +2,28 @@ package com.example.myemptyviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        val signIn = findViewById<TextView>(R.id.sigh_in_button)
+        val forgotPassword = findViewById<TextView>(R.id.forgot_password_textview)
+        val sighUp = findViewById<TextView>(R.id.sign_up_textview)
+
+        signIn.setOnClickListener {
+            Toast.makeText(this, getString(R.string.clicked_sign_in), Toast.LENGTH_LONG).show()
+        }
+        forgotPassword.setOnClickListener {
+            Toast.makeText(this, getString(R.string.clicked_forgot_password), Toast.LENGTH_LONG)
+                .show()
+        }
+        sighUp.setOnClickListener {
+            Toast.makeText(this, getString(R.string.clicked_sign_up), Toast.LENGTH_LONG).show()
+        }
     }
 }
