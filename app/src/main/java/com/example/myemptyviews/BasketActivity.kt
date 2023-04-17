a144d174119ffd5e48324ff0edf7c3bfd57ea26a
anddev404 <marcinek07@gmail.com>
2023-04-17 21:55:39 +0200

    set up action bar in BasketActivity

--- app/src/main/java/com/example/myemptyviews/BasketActivity.kt
+++ app/src/main/java/com/example/myemptyviews/BasketActivity.kt
@@ -1,13 +1,42 @@
 package com.example.myemptyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Html
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class BasketActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basket)
-        supportActionBar?.hide()
+        setUpActionBar()
 
+
+    }
+
+    private fun setUpActionBar() {
+
+        supportActionBar?.title =
+            Html.fromHtml(
+                "<font color=\"${resources.getColor(R.color.black)}\">${
+                    resources.getString(
+                        R.string.your_basket
+                    )
+                }</font>"
+            )
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.white)))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setHomeAsUpIndicator(com.google.android.material.R.drawable.ic_arrow_back_black_24);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
