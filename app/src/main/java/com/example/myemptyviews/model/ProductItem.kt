52956cfafc7adb8a4765008bbfec0570e7a688f8
anddev404 <marcinek07@gmail.com>
2023-04-22 20:43:44 +0200

    Create ProductItem.kt

---
+++ app/src/main/java/com/example/myemptyviews/model/ProductItem.kt
@@ -0,0 +1,5 @@
+package com.example.myemptyviews.model
+
+import android.graphics.drawable.Drawable
+
+data class ProductItem(var name: String, var amount: Int, var price: String, var image: Drawable)
