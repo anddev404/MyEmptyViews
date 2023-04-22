54b9d6a7cfd1bdf97cf79d2ad3f574d60783e0bc
anddev404 <marcinek07@gmail.com>
2023-04-22 22:17:33 +0200

    Update CalorieCounterActivity.kt

--- app/src/main/java/com/example/myemptyviews/CalorieCounterActivity.kt
+++ app/src/main/java/com/example/myemptyviews/CalorieCounterActivity.kt
@@ -7,5 +7,6 @@ class CalorieCounterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calorie_counter)
+        supportActionBar?.hide()
     }
 }
