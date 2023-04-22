49caf9f76340a61a00065231c3e2f4635c8a4b0c
anddev404 <marcinek07@gmail.com>
2023-04-22 21:17:55 +0200

    Update ProductItem.kt

--- app/src/main/java/com/example/myemptyviews/model/ProductItem.kt
+++ app/src/main/java/com/example/myemptyviews/model/ProductItem.kt
@@ -1,5 +1,47 @@
 package com.example.myemptyviews.model
 
+import android.app.Application
 import android.graphics.drawable.Drawable
+import com.example.myemptyviews.R
 
-data class ProductItem(var name: String, var amount: Int, var price: String, var image: Drawable)
+data class ProductItem(var name: String, var amount: Int, var price: String, var image: Drawable) {
+    companion object {
+
+        fun getProductList(application: Application): ArrayList<ProductItem> {
+
+            var list = ArrayList<ProductItem>()
+            val resources = application.resources
+
+            val drawableDonut = resources.getDrawable(R.drawable.image_donut)
+            val drawablePeas = resources.getDrawable(R.drawable.image_peas)
+            val drawableOranges = resources.getDrawable(R.drawable.image_orange)
+
+            list.add(
+                ProductItem(
+                    resources.getString(R.string.donut),
+                    resources.getInteger(R.integer.donut_pc),
+                    resources.getString(R.string.donut_price),
+                    drawableDonut
+                )
+            )
+            list.add(
+                ProductItem(
+                    resources.getString(R.string.peas),
+                    resources.getInteger(R.integer.peas_pc),
+                    resources.getString(R.string.peas_price_),
+                    drawablePeas
+                )
+            )
+            list.add(
+                ProductItem(
+                    resources.getString(R.string.oranges),
+                    resources.getInteger(R.integer.oranges_pc),
+                    resources.getString(R.string.oranges_price),
+                    drawableOranges
+                )
+            )
+            return list
+        }
+    }
+}
+
