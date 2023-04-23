18648f887cf33ecc46e31f3bef17e514e05fb2fa
anddev404 <marcinek07@gmail.com>
2023-04-23 22:41:20 +0200

    implement componentRecyclerView in CalorieCounterActivity

--- app/src/main/java/com/example/myemptyviews/CalorieCounterActivity.kt
+++ app/src/main/java/com/example/myemptyviews/CalorieCounterActivity.kt
@@ -1,12 +1,34 @@
 package com.example.myemptyviews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myemptyviews.model.ComponentItem
+import com.example.myemptyviews.view_adapters.ComponentRecyclerViewAdapter
 
 class CalorieCounterActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calorie_counter)
+        setUpActionBar()
+        setUpRecyclerView()
+    }
+
+    private fun setUpActionBar() {
         supportActionBar?.hide()
+    }
+
+    private fun setUpRecyclerView() {
+        recyclerView = findViewById(R.id.components_recycler_view)
+        recyclerView.setHasFixedSize(true);
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        var adapter = ComponentRecyclerViewAdapter(ComponentItem.getComponentList())
+        recyclerView.adapter = adapter
     }
 }
